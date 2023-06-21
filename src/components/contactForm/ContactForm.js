b10a36d82d3fc37597885fe87ce968f0475b4b8f
john feyisayo <johnfeyisayo@gmail.com>
2023-06-21 08:37:43 +0100

    update Contact Form

--- src/components/contactForm/ContactForm.js
+++ src/components/contactForm/ContactForm.js
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-// import emailjs from '@emailjs/browser';
 import classes from './ContactForm.module.css';
 import { useForm } from 'react-hook-form';
 
@@ -16,41 +15,10 @@ const ContactForm = () => {
     setMessage('Message Sent Successfully');
   };
 
-  // const form = useRef();
-
-  // const sendEmail = (e) => {
-  //   e.preventDefault();
-
-  //   emailjs.sendForm('service_zsctbsi', 'template_fsudj76', form.current, 'cIc6POE8KxFh268T8').then(
-  //     (result) => {
-  //       console.log(result.text);
-  //       e.target.reset();
-  //     },
-  //     (error) => {
-  //       console.log(error.text);
-  //     }
-  //   );
-  // };
   return (
     <div className={classes['contactform-container']}>
       <h1 className={classes['contactme-heading']}>Contact Me</h1>
       <div className={classes['sent-message']}>{message}</div>
-      {/* <form ref={form} onSubmit={sendEmail}>
-        <label>Name</label>
-        <input
-          type="text"
-          name="from_name"
-          {...register('fullname', { required: true })}
-          placeholder="full name"
-        />
-        {errors.fullname?.type === 'required' && 'Full Name is required'}
-        <label>Email</label>
-        <input type="email" name="user_email" placeholder="email..." />
-        <label>Message</label>
-        <textarea name="message" placeholder="message" />
-        <input type="submit" value="Send" />
-      </form> */}
-
       <form onSubmit={handleSubmit(onSubmit)}>
         <label>Name</label>
         <input
